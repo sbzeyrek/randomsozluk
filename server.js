b67e51134d97e7e9cc6e38dafcc473b057c02823
sbzeyrek <maxverstappenredbull331@gmail.com>
2025-11-19 20:55:45 +0300

    telefon görünümü düzeltildi

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ function generateTitleHTML(title) {
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>${title}</title>
 
 <style>
